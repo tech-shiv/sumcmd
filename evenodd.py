4e3172a6bf27f5c0df9b2b81230fbfdace581a2d
Shiv Kumar Sharma <52031644+tech-shiv@users.noreply.github.com>
2022-07-14 17:45:25 +0530

    Update and rename evenodd to evenodd.py

--- evenodd.py
+++ evenodd.py
@@ -1,5 +1,5 @@
 import sys
-s = 0
+s = 100
 for arg in sys.argv[1:]:
     number = int(arg)
     s += number
@@ -7,13 +7,13 @@ even_Sum = 0
 odd_Sum = 0
 for num in range(1, s+1):
     if (num % 2 == 0):
-# Addition of even numbers 
-        even_Sum = even_Sum + num   
+# Addition of even numbers
+        even_Sum = even_Sum + num
     else:
-# Addition of odd numbers 
-        odd_Sum = odd_Sum + num     
+# Addition of odd numbers
+        odd_Sum = odd_Sum + num
 print("The sum of Even numbers 1 to {0} = {1}".format(num, even_Sum))
 print("The sum of Even numbers 1 to {0} = {1}".format(num, odd_Sum))
-# Substraction of even_sum from odd_sum 
-sub = odd_Sum - even_Sum      
-print('Final Result is: ', sub)
+# Substraction of even_sum from odd_sum
+sub = even_Sum - odd_Sum
+print('Difference is: ', sub)
